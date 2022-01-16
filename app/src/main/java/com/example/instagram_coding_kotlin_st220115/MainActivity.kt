5cc0cb5cfeb0e97002b1e529723dcb652608090c
HIBER97 <hibar9710@gmail.com>
2022-01-16 13:44:40 +0900

    Update 22.01.16 13:44 ------------------------------- Add: Add "User page" xml ------------------------------- Fix :
    -------------------------------
    Error (Have to Fix) :
    -Overall-
    App keeps stopping
    
    -activity_login-
    Null
    
    -------------------------------

--- app/src/main/java/com/example/instagram_coding_kotlin_st220115/MainActivity.kt
+++ app/src/main/java/com/example/instagram_coding_kotlin_st220115/MainActivity.kt
@@ -1,11 +1,78 @@
-package com.example.instagram_coding_kotlin_st220115
+package com.example.instagramcodingkotlin
 
+import android.content.Intent
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.company.howl.howlstagram.navigation.*
+import com.google.android.material.navigation.NavigationView
+import java.util.jar.Manifest
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener{
+    override fun onNavigationItemSelected(p0: MenuItem): Boolean {
+        setToolbarDefault()
+        when (item.itemId) {
+            R.id.action_home -> {
+
+                val detailViewFragment = DetailViewFragment()
+                supportFragmentManager.beginTransaction()
+                    .replace(R.id.main_content, detailViewFragment)
+                    .commit()
+                return true
+            }
+            R.id.action_search -> {
+                val gridFragment = GridFragment()
+                supportFragmentManager
+                    .beginTransaction()
+                    .replace(R.id.main_content, gridFragment)
+                    .commit()
+                return true
+            }
+            R.id.action_add_photo -> {
+                if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
+                    startActivity(Intent(this, AddPhotoActivity::class.java))
+                } else {
+                    Toast.makeText(this, "스토리지 읽기 권한이 없습니다.", Toast.LENGTH_LONG).show()
+                }
+                return true
+            }
+            R.id.action_favorite_alarm -> {
+                val alarmFragment = AlarmFragment()
+                supportFragmentManager
+                    .beginTransaction()
+                    .replace(R.id.main_content, alarmFragment)
+                    .commit()
+                return true
+            }
+            R.id.action_account -> {
+                val userFragment = UserFragment()
+                val uid = FirebaseAuth.getInstance().currentUser!!.uid
+                val bundle = Bundle()
+                bundle.putString("destinationUid", uid)
+                userFragment.arguments = bundle
+                supportFragmentManager.beginTransaction()
+                    .replace(R.id.main_content, userFragment)
+                    .commit()
+                return true
+            }
+        }
+        return false
+    }
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        R.menu.bottom_navigation_main.setOnNavigationItemSelectedListener(this)
+        ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE), 1)
+
+        R.menu.bottom_navigation_main.selectedItemId = R.id.action_home
     }
+
+
 }
+
--- app/src/test/java/instagramcodingkotlin/navigation/UserFragment.kt
+++ app/src/test/java/instagramcodingkotlin/navigation/UserFragment.kt
@@ -1,27 +1,132 @@
 package com.company.howl.howlstagram.navigation
 
-
+import android.Manifest
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.PorterDuff
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.support.v4.app.ActivityCompat
+import android.support.v4.app.Fragment
+import android.support.v4.content.ContextCompat
+import android.support.v7.widget.GridLayoutManager
+import android.support.v7.widget.LinearLayoutCompat
+import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.appcompat.widget.LinearLayoutCompat
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
+import com.company.howl.howlstagram.LoginActivity
+import com.company.howl.howlstagram.MainActivity
+import com.company.howl.howlstagram.R
+import com.company.howl.howlstagram.R.id.account_tv_post_count
+import com.company.howl.howlstagram.model.AlarmDTO
+
+import com.company.howl.howlstagram.model.ContentDTO
+import com.company.howl.howlstagram.model.FollowDTO
+import com.company.howl.howlstagram.util.FcmPush
 import com.example.instagramcodingkotlin.R
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.FirebaseUser
-import java.util.*
-
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.ListenerRegistration
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.fragment_user.*
+import kotlinx.android.synthetic.main.fragment_user.view.*
+import java.util.ArrayList
 
 class UserFragment : Fragment() {
 
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        var view = LayoutInflater.from(activity).inflate(R.layout.fragment_user, container, false)
+    // Firebase
+    var auth: FirebaseAuth? = null
+    var firestore: FirebaseFirestore? = null
 
-        return view
+    //private String destinationUid;
+    var uid: String? = null
+    var currentUserUid: String? = null
+    var fragmentView: View? = null
+
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?) {
+
+        fragmentView = inflater.inflate(R.layout.fragment_user, container, false)
+        uid = arguments!!.getString("destinationUid")
+        // Firebase
+        auth = FirebaseAuth.getInstance()
+        firestore = FirebaseFirestore.getInstance()
+        fcmPush = FcmPush()
+
+        currentUserUid = auth?.currentUser?.uid
+
+        fragmentView?.account_reyclerview?.adapter = UserFragmentRecyclerViewAdapter()
+        fragmentView?.account_reyclerview?.layoutManger = GridLayoutManager(activity!!,3)
+
+        }
+
+    inner class UserFragmentRecyclerViewAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+
+        val contentDTOs: ArrayList<ContentDTO>
+
+        init {
+
+            contentDTOs = ArrayList()
+
+            // 나의 사진만 찾기
+            recyclerListenerRegistration = firestore?.collection("images")?.whereEqualTo("uid", uid)?.addSnapshotListener { querySnapshot, firebaseFirestoreException ->
+                contentDTOs.clear()
+                if (querySnapshot == null) return@addSnapshotListener
+                for (snapshot in querySnapshot?.documents!!) {
+                    contentDTOs.add(snapshot.toObject(ContentDTO::class.java)!!)
+                }
+
+                fragmentView?.account_tv_post_count.text = contentDTOs.size.toString()
+                notifyDataSetChanged()
+            }
+
+        }
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+
+            val width = resources.displayMetrics.widthPixels / 3
+
+            val imageView = ImageView(parent.context)
+            imageView.layoutParams = LinearLayoutCompat.LayoutParams(width, width)
+
+            return CustomViewHolder(imageView)
+        }
+
+        override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+            var imageview = (holder as CustomViewHolder).imageView
+            Glide.with(holder.itemView.context)
+                .load(contentDTOs[position].imageUrl)
+                .apply(RequestOptions().centerCrop())
+                .into(imageview)
+        }
+
+        override fun getItemCount(): Int {
+
+            return contentDTOs.size
+        }
+
+        override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int, payloads: MutableList<Any>) {
+            super.onBindViewHolder(holder, position, payloads
+                var imageview = (p0 as CustomViewHolder).imageview
+            Glide.with(p0.itemView.context).load(contentDTOs[p1].imageUrl).apply(RequestOptions().centerCrop()).into(imageview)
+        }
+
+        // RecyclerView Adapter - View Holder
+        inner class CustomViewHolder(var imageView: ImageView) : RecyclerView.ViewHolder(imageView)
     }
+
+    override fun onStop() {
+        super.onStop()
+        followListenerRegistration?.remove()
+        followingListenerRegistration?.remove()
+        imageprofileListenerRegistration?.remove()
+        recyclerListenerRegistration?.remove()
+    }
+
 }
